057d49ff5e387f684f603098e552e04980e9b4ca
Jonas Kazlauskas <jonas.kazlauskas6@gmail.com>
2023-02-18 23:39:22 +0200

    commitment

---
+++ src/main/java/sanciai/EmployeeDataTool/EmployeeDataToolApplication.java
@@ -0,0 +1,13 @@
+package sanciai.EmployeeDataTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDataToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sanciai/EmployeeDataTool/EmployeeDataToolApplicationTests.java
@@ -0,0 +1,13 @@
+package sanciai.EmployeeDataTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDataToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
