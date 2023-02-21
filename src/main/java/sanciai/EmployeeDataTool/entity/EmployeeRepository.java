fbb6313256071ebc1f3b0d3d994960d9dff96ee2
feliciyus <jonas.kazlauskas6@gmail.com>
2023-02-21 23:36:43 +0200

    removed ignored files

--- src/main/java/sanciai/EmployeeDataTool/entity/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package sanciai.EmployeeDataTool.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-// CrudRepository<Employee, Long>
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO employee_data (Name, Email, Phone_Number) VALUES ('Jonas Kazlauskas', 'jonas@gmail.com', '952512');
-
-INSERT INTO employee_data (Name, Email, Phone_Number) VALUES ('Augustas Vilcinskas', 'Augustas@gmail.com', '9444352512');
